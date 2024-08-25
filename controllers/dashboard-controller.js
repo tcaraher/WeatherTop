0c401260d6d26d5d80bd398a61851446af9c50e3
Tom Caraher <info@tomcaraher.com>
2024-08-25 21:19:01 +0100

    added alphabetical sort of stations on dashboard

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -7,21 +7,25 @@ export const dashboardController = {
   async index(request, response) {
     const loggedInUser = await accountsController.getLoggedInUser(request);
     const stations = await stationStore.getStationsByUserId(loggedInUser._id);
+    // const stations = Object.keys(unsortedStations).sort((a,b) => unsortedStations[a] - unsortedStations[b]);
+
 
     for (const station of stations) {
       const stationReports = await stationStore.getStationById(station._id);
       station.recentReport = stationReports.reports[0];
       if (stationReports.reports.length > 0) {
-      station.reportAnalytics = {
-        maxTemp: reportAnalytics.getMax("temperature", stationReports.reports),
-        minTemp: reportAnalytics.getMin("temperature", stationReports.reports),
-        maxWindSpeed: reportAnalytics.getMax("windspeed", stationReports.reports),
-        minWindSpeed: reportAnalytics.getMin("windspeed", stationReports.reports),
-        maxPressure: reportAnalytics.getMax("pressure", stationReports.reports),
-        minPressure: reportAnalytics.getMin("pressure", stationReports.reports),
-      };
+        station.reportAnalytics = {
+          maxTemp: reportAnalytics.getMax("temperature", stationReports.reports),
+          minTemp: reportAnalytics.getMin("temperature", stationReports.reports),
+          maxWindSpeed: reportAnalytics.getMax("windspeed", stationReports.reports),
+          minWindSpeed: reportAnalytics.getMin("windspeed", stationReports.reports),
+          maxPressure: reportAnalytics.getMax("pressure", stationReports.reports),
+          minPressure: reportAnalytics.getMin("pressure", stationReports.reports),
+        };
+      }
     }
-    }
+    // https://www.freecodecamp.org/news/how-to-sort-array-of-objects-by-property-name-in-javascript/
+    stations.sort((a, b) => a.station.localeCompare(b.station));
 
     const viewData = {
       title: "Weather Top Dashboard",
